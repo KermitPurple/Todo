476e960260a10c0f49bad8e7dc6abb29933ede9d
Shane McDonough <shanemcdman@gmail.com>
2020-02-05 13:22:16 -0800

    Create cleanlist and getlist

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-def getlist():
+def cleanlist(inputlst):
     lst = []
-    with open("list.txt"):
-        
+    for item in inputlst:
+        lst.append(item[:-1])
+    return lst
 
+def getlist():
+    with open("list.txt", 'r') as l:
+        lst = l.readlines()
+        lst = cleanlist(lst)
+    return lst
 
 def main():
-    print("test")
+    print(getlist())
 
 if __name__ == "__main__":
     main()
