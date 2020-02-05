9fe760c5cafb6fa14b4be1437a8ea9e414bbca9a
Shane McDonough <shanemcdman@gmail.com>
2020-02-05 13:30:59 -0800

    Create menu and updatelist

--- main.py
+++ main.py
@@ -10,9 +10,29 @@ def getlist():
         lst = cleanlist(lst)
     return lst
 
+def updatelist(inputlst):
+    with open("list.txt", 'w') as l:
+        for item in inputlst:
+            l.write(item + "\n")
+
+def printlist(inputlst):
+    pass
+
+def additem(inputlst):
+    pass
+
+def deleteitem(inputlst):
+    pass
+
+def menu():
+    print("1) Print list")
+    print("2) Add item")
+    print("3) Delete item")
+    print("4) quit")
+
 def main():
-    print(getlist())
+    lst = getlist()
+    updatelist(lst)
 
 if __name__ == "__main__":
     main()
-
